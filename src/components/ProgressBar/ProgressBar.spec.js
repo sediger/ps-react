8314e74a9f3a1debfc1558d38ca094c59047a4e5
Shemiah <shemiah@medlor.com>
2018-09-20 15:31:54 -0700

    Project Completion

---
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ProgressBar from './ProgressBar';
+
+describe('ProgressBar', () => {
+  test('getWidthAsPercentOfTotalWidth should return 250 with total width of 500 and percent of 50', () => {
+      const wrapper = shallow(<ProgressBar percent={50} width={500} />);
+      const width = wrapper.instance().getWidthAsPercentOfTotalWidth();
+      /*const progBar50 = Example50Percent();
+      const width = parseInt(progBar50.props.width * (progBar50.props.percent / 100), 10);*/
+      expect(width).toEqual(250);
+  });
+});
+
+function Example50Percent() {
+  return <ProgressBar percent={50} width={500} />
+}
